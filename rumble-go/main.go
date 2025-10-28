ed9c1c63902e0399b50568c24a39e22d24f056e0
Matt P <mattp3259@gmail.com>
2025-10-28 16:40:01 -0500

    move stuff

--- rumble-go/main.go
+++ rumble-go/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hi world!")
+	filename := "SE1.TRK"
+	file, err := os.Open(filename)
+
+	if err != nil {
+		log.Fatalf("Failed to open file: %v", err)
+	}
+
+	defer file.Close()
+
+	info, err := file.Stat()
+
+	if err != nil {
+		log.Fatalf("Failed to get file info: %v", err)
+	}
+
+	fmt.Printf("File: %s\nSize: %d bytes\n\n", info.Name(), info.Size())
+
 }
