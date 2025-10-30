610bbea2efea72b9661786d7ab2bf6943c080e2d
Matt P <mattp3259@gmail.com>
2025-10-30 14:19:24 -0500

    add shoc struct

--- rumble-go/chunk/chunk.go
+++ rumble-go/chunk/chunk.go
@@ -36,59 +36,99 @@ func Print(c TopLevelChunk, doHex bool) {
 	}
 }
 
-func readTopLevelChunk(r io.ReadSeeker) (chunk TopLevelChunk, err error) {
-	startPos, _ := r.Seek(0, io.SeekCurrent)
+func readTopLevelChunk(r io.ReadSeeker) (TopLevelChunk, error) {
+	startPosSigned, _ := r.Seek(0, io.SeekCurrent)
+	startPos := uint32(startPosSigned)
 
-	var tag [4]byte
-	if _, err = io.ReadFull(r, tag[:]); err != nil {
-		return Chunk{}, err
+	tag := make([]byte, 4)
+	if _, err := io.ReadFull(r, tag); err != nil {
+		return nil, err
 	}
 
-	// Reverse FOURCC bytes since it's stored little-endian
+	// Reverse bytes if little-endian
 	for i := 0; i < 2; i++ {
 		tag[i], tag[3-i] = tag[3-i], tag[i]
 	}
-
-	fourcc := string(tag[:])
+	fourcc := string(tag)
 
 	pos, _ := r.Seek(0, io.SeekCurrent)
 
-	// If this is a FILL chunk on a 0x6000 boundary, just return no data
-	if fourcc == "FILL" && ((pos % 0x6000) == 0) {
-		return Chunk{
-			FourCC:     fourcc,
-			ChunkStart: startPos,
-			ChunkSize:  0,
-			Data:       make([]byte, 0),
-		}, err
+	switch fourcc {
+	case "CTRL":
+		return readCTRLChunk(r, startPos)
+	case "SHOC":
+		return readSHOCChunk(r, startPos)
+	case "FILL":
+		return readFILLChunk(r, startPos, pos)
+	default:
+		panic("Unrecognized top level chunk: " + fourcc)
 	}
+}
+
+func readCTRLChunk(r io.ReadSeeker, startPos uint32) (TopLevelChunk, error) {
+	var chunkSize uint32
+	if err := binary.Read(r, binary.LittleEndian, &chunkSize); err != nil {
+		return nil, err
+	}
+
+	data := make([]byte, chunkSize-8)
+	if _, err := io.ReadFull(r, data); err != nil {
+		return nil, err
+	}
+
+	return &Ctrl{
+		index:        0,
+		fourCC:       "CTRL",
+		startAddress: startPos,
+		data:         data,
+	}, nil
+}
+
+func readSHOCChunk(r io.ReadSeeker, startPos uint32) (*Shoc, error) {
+	var chunkSize uint32
+	if err := binary.Read(r, binary.LittleEndian, &chunkSize); err != nil {
+		return nil, err
+	}
+
+	data := make([]byte, chunkSize-8)
+	if _, err := io.ReadFull(r, data); err != nil {
+		return nil, err
+	}
+
+	return &Shoc{
+		index:        0,
+		fourCC:       "SHOC",
+		startAddress: startPos,
+		data:         data,
+	}, nil
+}
+
+func readFILLChunk(r io.ReadSeeker, startPos uint32, pos int64) (TopLevelChunk, error) {
+	// Handle special case: fill tag ends on 0x6000 boundary
+	if pos%0x6000 == 0 {
+		return &Fill{
+			fourCC:       "FILL",
+			startAddress: startPos,
+			data:         []byte{},
+		}, nil
+	}
+
+	// Normal FILL parsing
 
 	var chunkSize uint32
-	var data []byte
-
-	// read chunk size in bytes (second u32)
-	if err = binary.Read(r, binary.LittleEndian, &chunkSize); err != nil {
-		return Chunk{}, err
+	if err := binary.Read(r, binary.LittleEndian, &chunkSize); err != nil {
+		return nil, err
 	}
 
-	dataSize := chunkSize - 8
+	data := make([]byte, chunkSize-8)
 
-	data = make([]byte, dataSize)
-
-	if _, err = io.ReadFull(r, data); err != nil {
-		return Chunk{}, err
+	if _, err := io.ReadFull(r, data); err != nil {
+		return nil, err
 	}
 
-	// fmt.Println(hex.Dump(data))
-
-	// fmt.Printf("Size (data): %d\n", len(data))
-	// fmt.Printf("Size (decimal): %d\n", chunkSize)
-
-	return Chunk{
-		FourCC:     fourcc,
-		ChunkStart: startPos,
-		ChunkSize:  chunkSize,
-		Data:       data,
+	return &Fill{
+		fourCC:       "FILL",
+		startAddress: startPos,
+		data:         data,
 	}, nil
-	// return fourcc, chunkSize, data, nil
 }
--- rumble-go/chunk/shoc.go
+++ rumble-go/chunk/shoc.go
@@ -1,7 +1,29 @@
 package chunk
 
-func ParseShoc(c *Chunk) []Chunk {
-	println("parse SHOC!")
+type Shoc struct {
+	index        uint32
+	fourCC       string
+	startAddress uint32
+	data         []byte
+}
 
-	return make([]Chunk, 0)
+func (c *Shoc) FourCC() string {
+	return c.fourCC
+}
+
+func (c *Shoc) TotalSize() uint32 {
+	return uint32(len(c.data))
+}
+
+func (c *Shoc) StartAddress() uint32 {
+	return c.startAddress
+}
+
+func (c *Shoc) Data() []byte {
+	return c.data
+}
+
+// 3. Implement TopLevelChunk interface method
+func (c *Shoc) Index() uint32 {
+	return c.index
 }
