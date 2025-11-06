dd41198afe366fa4468fba5fb624789b7c66da37
Matt P <mattp3259@gmail.com>
2025-11-06 10:23:08 -0600

    cmd example

---
+++ rumble-reader/cmd/greet.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var greetCmd = &cobra.Command{
+	Use:   "greet [name]",
+	Short: "Print a friendly greeting",
+	Args:  cobra.MaximumNArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		name := "world"
+		if len(args) == 1 {
+			name = args[0]
+		}
+		fmt.Printf("Hello, %s!\n", name)
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(greetCmd)
+}
