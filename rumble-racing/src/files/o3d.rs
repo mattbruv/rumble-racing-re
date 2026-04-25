a57d5e61dfad08121476ec1f12d37bafdc070805
Matt P <mattp3259@gmail.com>
2026-04-25 12:23:49 -0400

    read chicken

--- rumble-racing/src/files/o3d.rs
+++ rumble-racing/src/files/o3d.rs
@@ -1,3 +1,3 @@
-pub fn hi() {
-    println!("HI");
+pub fn parse_o3d(binary: &[u8]) {
+    println!("len: {}", binary.len());
 }
--- rumble-racing/src/main.rs
+++ rumble-racing/src/main.rs
@@ -1,5 +1,9 @@
+use std::fs;
 mod files;
 
 fn main() {
-    files::o3d::hi();
+    let file =
+        fs::read("../OUT-FEB-7/SE1 - True Grits/o3d/23_SOURCES-SE_CHICKENA.O3D.o3d").unwrap();
+
+    files::o3d::parse_o3d(&file);
 }
