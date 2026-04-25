07aaaac643ce420312ddabb221b405297b8028f6
Matt P <mattp3259@gmail.com>
2026-04-25 13:25:25 -0400

    asdf

--- rumble-racing/src/files/chunk.rs
+++ rumble-racing/src/files/chunk.rs
@@ -1,4 +1,7 @@
-use std::string::FromUtf8Error;
+use std::{
+    io::{self, Cursor, Read},
+    string::FromUtf8Error,
+};
 
 use thiserror::Error;
 
@@ -7,6 +10,9 @@ use crate::files::types::FourCC;
 pub enum GenericChunkParseError {
     #[error("Bad FourCC")]
     InvalidFourCC(#[from] FromUtf8Error),
+
+    #[error("IO error")]
+    IoError(#[from] io::Error),
 }
 
 #[derive(Debug)]
@@ -17,6 +23,15 @@ pub struct GenericChunk<'a> {
 
 pub fn parse_generic_chunks(data: &[u8]) -> Result<Vec<GenericChunk<'_>>, GenericChunkParseError> {
     let mut chunks = Vec::new();
+
+    let mut cursor = Cursor::new(data);
+
+    let mut tag_buffer: [u8; 4] = [0; 4];
+    cursor.read_exact(&mut tag_buffer)?; // automatically advances cursor position
+    tag_buffer.reverse(); // the tags are backwards, so reverse them
+    let fourcc = FourCC::new(tag_buffer)?;
+
+    println!("BUFFER: {:?}", fourcc);
 
     Ok(chunks)
 }
