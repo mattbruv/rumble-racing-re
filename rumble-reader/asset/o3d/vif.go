c3eff649c139c5e03e1cb17431c42a8522306d8c
Matt P <mattp3259@gmail.com>
2026-05-06 22:08:13 -0400

    show draw

--- rumble-reader/asset/o3d/vif.go
+++ rumble-reader/asset/o3d/vif.go
@@ -265,12 +265,13 @@ func (elda *ELDA_Data) ParseVif() (*ParsedVif, error) {
 					if err := binary.Read(reader, binary.LittleEndian, &raw3); err != nil {
 						return nil, err
 					}
+					draw := (raw3 & 0b1) == 0b1
 					unpack.V3_32 = append(unpack.V3_32, V3_32Entry{
 						V1:        math.Float32frombits(raw1),
 						V2:        math.Float32frombits(raw2),
 						V3:        math.Float32frombits(raw3),
-						ADCBitSet: (raw3 & 0b1) == 0b1,
-						Debug:     fmt.Sprintf("offset: %d, row regs: %v mask: 0x%08X", entryOffset, state.rowRegisters, state.maskRegister),
+						ADCBitSet: draw,
+						Debug:     fmt.Sprintf("draw: %v, offset: %d, row regs: %v mask: 0x%08X", draw, entryOffset, state.rowRegisters, state.maskRegister),
 					})
 				}
 
