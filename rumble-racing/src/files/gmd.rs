8bba7e72d80efa5b213d516c3f086c0836ded82e
Matt P <mattp3259@gmail.com>
2026-05-12 22:36:45 -0400

    remove prototype rust program

--- rumble-racing/src/files/gmd.rs
+++
@@ -1,8 +0,0 @@
-// use crate::files::chunk::GenericChunk;
-
-#[derive(Debug)]
-pub struct Gmd {}
-
-// pub fn parse_gmd(chunk: GenericChunk) -> Gmd {
-//     Gmd {}
-// }
--- rumble-racing/src/files/mod.rs
+++
@@ -1,6 +0,0 @@
-mod chunk;
-pub mod gmd;
-pub mod o3d;
-pub mod obf;
-pub mod types;
-pub mod vif;
